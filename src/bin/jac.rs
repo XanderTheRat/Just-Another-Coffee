12113275f02ed29e1086c5f96fbbcc05a831f523
XanderTheRat <saussic0canard@gmail.com>
2026-09-14 11:29:54 +0200

    Add: Ask if user want a coffee

--- src/bin/jac.rs
+++ src/bin/jac.rs
@@ -1,9 +1,20 @@
+use std::io;
 use std::fs;
 
-fn main() {
-    let content = fs::read_to_string("coffee.txt");
-    match content {
-        Ok(f) => println!("Want a coffee ?\n{}", f),
-        Err(_e) => println!("No more coffee."),
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let mut buffer = String::new();
+    println!("Want a coffee ?");
+    io::stdin().read_line(&mut buffer)?;
+    if !(buffer.to_lowercase() == "n") {
+        let content = fs::read_to_string("coffee");
+        match content {
+            Ok(f) => println!("\n{}", f),
+            Err(e) => println!("No more coffee in cup : \n{}", e),
+        };
+    }
+    else {
+        println!("No more coffee.")
+    }
+    Ok(())
+
 }
