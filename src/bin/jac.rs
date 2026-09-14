56ddc735db88936d6f7386684b06473905a25453
XanderTheRat <saussic0canard@gmail.com>
2026-09-14 18:37:41 +0200

    Add : An option allow user to color the coffee following the pixel\'s position

--- src/bin/jac.rs
+++ src/bin/jac.rs
@@ -1,10 +1,24 @@
 use clap::Parser;
+use colored::Colorize;
 use std::io;
 
 
-fn print_coffee() {
+fn print_coffee(gm: bool) {
     const ASCII : &str = include_str!("../../coffee");
-    println!("{}", ASCII);
+    if !(gm) {
+        println!("{}", ASCII);
+    } else {
+        for (y, line) in ASCII.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                let r = (x.saturating_mul(4) % 255) as u8;
+                let g = (y.saturating_mul(12) % 255) as u8;
+                let b = 150; 
+
+                print!("{}", c.to_string().truecolor(r, g, b));
+            }
+            println!();
+        }
+    }
 }
 
 fn show_details() {
@@ -13,11 +27,13 @@ fn show_details() {
     details += "usages : jac [option]";
     details += "If you want a coffee, the script print a ascii coffee to remind you to drink one.\n\n";
     // -q
-    details += "-q      --question  :   Show a question asking user to drink a coffee ( unactive by default )\n";
+    details += "-q      --question      :   Show a question asking user to drink a coffee ( unactive by default )\n";
     // -y
-    details += "-y      --yes   :   Skip the question by auto-responding \"yes\"\n";
+    details += "-y      --yes           :   Skip the question by auto-responding \"yes\"\n";
     // -n
-    details += "-n      --no    :   Skip the question by auto-responding \"no\"\n";
+    details += "-n      --no            :   Skip the question by auto-responding \"no\"\n";
+    // --graduate_multicolor
+    details += "--graduate-multicolor   :   Color the ascii output based on the position (x, y) of the pixel\n";
     // -h
     details += "-h      --help  :   Show arguments and usage of the command"; 
 
@@ -27,22 +43,23 @@ fn show_details() {
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut buffer = String::new();
     let args: Args = Args::parse();
+    println!("{:?}", args);
 
     if args.help{
         show_details();
     }
     else if !(args.question) && !(args.no) {
-        print_coffee();
+        print_coffee(args.graduate_multicolor);
     }
     else if !(args.no) {
         if args.yes {
-            print_coffee();
+            print_coffee(args.graduate_multicolor);
         }else {
             println!("Want a coffee ?");
             io::stdin().read_line(&mut buffer)?;
 
             if !(buffer.trim().to_lowercase() == "n") {
-                print_coffee();
+                print_coffee(args.graduate_multicolor);
             }
             else {
                 println!("No more coffee.");
@@ -76,5 +93,8 @@ struct Args {
     yes : bool,
     
     #[arg(short, long)]
-    help : bool
+    help : bool,
+
+    #[arg(short, long)]
+    graduate_multicolor : bool,
 }
